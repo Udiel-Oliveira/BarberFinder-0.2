c72583549ec25b675b87c041b16dc94708951b96
Udiel-Oliveira <udiel.inaciosilva@gmail.com>
2024-08-14 21:19:00 -0300

    Feat: splash screen

--- src/Screens/Preload/index.js
+++ src/Screens/Preload/index.js
@@ -1,11 +1,30 @@
-import React from 'react';
-import {Container} from './styles';
-import {Text} from 'react-native';
+import React, {useEffect} from 'react';
+import {Container, NameLogo, LoadingIcon} from './styles';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useNavigation} from '@react-navigation/native';
+
+import BarberLogo from '../../assets/Logo.svg';
 
 export default () => {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const checkToken = async () => {
+      const token = await AsyncStorage.getItem('token');
+      if (token !== null) {
+        //validar o tokens
+      } else {
+        navigation.navigate('SignIn');
+      }
+    };
+    checkToken();
+  }, []);
+
   return (
     <Container>
-      <Text>Preload</Text>
+      <BarberLogo width="100%" />
+      <NameLogo width="100%">BarBerFinder</NameLogo>
+      <LoadingIcon size="large" color="#FFFFFF" />
     </Container>
   );
 };
