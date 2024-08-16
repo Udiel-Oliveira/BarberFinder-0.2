2a83b77086e68cf8ea1a30d0e508a7206e0de42c
Udiel-Oliveira <udiel.inaciosilva@gmail.com>
2024-08-15 22:17:33 -0300

    Feat: tela de Login

--- src/Screens/SignIn/index.js
+++ src/Screens/SignIn/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   Container,
   NameLogo,
@@ -8,20 +8,46 @@ import {
   SignMessageButton,
   SignMessageButtonText,
   SignMessageButtonTextBold,
+  LogoContainer,
+  Saudacao,
+  NameLogin,
 } from './styles';
 import {SafeAreaView} from 'react-native-safe-area-context';
 
+import SignInput from '../../components/SignInput';
+
 import BarberLogo from '../../assets/Logo-black.svg';
+import EmailIcon from '../../assets/email.svg';
+import LockIcon from '../../assets/lock.svg';
 
 export default () => {
+  const [emailFild, setEmailFild] = useState('karalho@karalho');
+  const [senhaFild, setSenhaFild] = useState('');
+
   return (
     <Container>
-      <BarberLogo width="100%" heith="160px" color="#000000" />
-      <NameLogo width="100%" font-size="35px">
-        BarBerFinder
-      </NameLogo>
+      <LogoContainer>
+        <BarberLogo width="10%" color="#000000" />
+        <NameLogo font-size="15px">BarBerFinder</NameLogo>
+      </LogoContainer>
 
       <InputArea>
+        <Saudacao>Bem Vindo</Saudacao>
+        <NameLogin>Email</NameLogin>
+        <SignInput
+          IconSvg={EmailIcon}
+          placeholder="Digite seu Email"
+          value={emailFild}
+          onChangeText={t => setEmailFild(t)}
+        />
+        <NameLogin>Senha</NameLogin>
+        <SignInput
+          IconSvg={LockIcon}
+          placeholder="Digite suas Senha"
+          value={senhaFild}
+          onChangeText={t => setSenhaFild(t)}
+          senha={true}
+        />
         <CustomButton>
           <CustomButtonText>Login</CustomButtonText>
         </CustomButton>
--- src/Screens/SignIn/styles.js
+++ src/Screens/SignIn/styles.js
@@ -4,27 +4,66 @@ import styled from 'styled-components/native';
 export const Container = styled.SafeAreaView`
   background-color: #ffffff;
   flex: 1;
-  justify-content: center;
   align-items: center;
 `;
+
 export const NameLogo = styled.Text`
   color: #000000;
-  font-size: 35px;
-  margin-top: 20px;
+  font-size: 15px;
   font-weight: 900;
   text-align: center;
 `;
+export const Saudacao = styled.Text`
+  color: #000000;
+  font-weight: 900;
+  font-size: 35px;
+  margin-bottom: 20px;
+`;
+export const NameLogin = styled.Text`
+  color: #000000;
+  font-size: 20px;
+  font-weight: 900;
+`;
 export const InputArea = styled.View`
   padding: 40px;
+  width: 100%;
+  flex: 1;
 `;
-export const CustomButton = styled.TouchableOpacity``;
+export const LogoContainer = styled.View`
+  width: 100%;
+  height: 20%;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  flex-direction: row;
+`;
+export const CustomButton = styled.TouchableOpacity`
+  height: 50px;
+  background-color: #176b70;
+  border-radius: 5px;
+  justify-content: center;
+  align-items: center;
+`;
 export const CustomButtonText = styled.Text`
-  color: #000000;
+  color: #ffffff;
+  font-size: 18px;
 `;
-export const SignMessageButton = styled.TouchableOpacity``;
+export const SignMessageButton = styled.TouchableOpacity`
+  flex-direction: row;
+  align-items: end;
+  margin-top: 50px;
+  margin-bottom: 20px;
+  justify-content: center;
+  align-items: flex-end;
+  flex: 1;
+`;
 export const SignMessageButtonText = styled.Text`
-  color: #000000;
+  color: #176b70;
+  font-size: 12px;
 `;
 export const SignMessageButtonTextBold = styled.Text`
-  color: #000000;
+  color: #176b70;
+  font-size: 12px;
+  font-weight: 900;
+  margin-left: 5px;
 `;
